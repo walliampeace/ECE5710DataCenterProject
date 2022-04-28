2069d14cdcdb763ef6f41e2864bc9d90cfa140bd
walliampeace <xinyu@DESKTOP-63ODSJV.localdomain>
2022-04-28 17:29:28 -0400

    project update

---
+++ python-microservices/admin/products/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://mvrcbrxu:LxlVQV2HYAv_HxaampDXlRYpyf1m7lV6@moose.rmq.cloudamqp.com/mvrcbrxu?heartbeat=800')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
