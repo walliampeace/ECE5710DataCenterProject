d24f34804ab5f51359918593bae2ee2be96a4194
Xinyu Wu <59054117+walliampeace@users.noreply.github.com>
2022-04-28 17:47:13 -0400

    Update producer.py

--- python-microservices/main/producer.py
+++ python-microservices/main/producer.py
@@ -1,6 +1,6 @@
 import pika, json
 
-params = pika.URLParameters('amqps://mvrcbrxu:LxlVQV2HYAv_HxaampDXlRYpyf1m7lV6@moose.rmq.cloudamqp.com/mvrcbrxu?heartbeat=800')
+params = pika.URLParameters('your_rabbitmq_url')
 
 connection = pika.BlockingConnection(params)
 
